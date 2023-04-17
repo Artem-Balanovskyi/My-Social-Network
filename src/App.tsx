baa2080e6f54cac2da03e6eeed4c08bc142cde30
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-04-17 13:26:07 +0300

    feat: add basic project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import Profile from "./Components/Profile/Profile";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
+        </div>);
 }
 
 export default App;
