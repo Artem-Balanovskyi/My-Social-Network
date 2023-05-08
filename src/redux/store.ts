65dd474e1f5b57a1823fbb78af0d5f465c38c1f4
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-05-08 22:29:50 +0300

    update: update posts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -28,7 +28,8 @@ export const store: IStore = {
                 {id: 1, message: 'Hi, how are you?', likesCount: 12},
                 {id: 2, message: 'It\'s my first post', likesCount: 11},
                 {id: 3, message: 'Blabla', likesCount: 11},
-                {id: 4, message: 'Dada', likesCount: 11}
+                {id: 4, message: 'Dada', likesCount: 11},
+                {id: 5, message: 'Hey!', likesCount: 13}
             ],
             newPostText: '',
 
