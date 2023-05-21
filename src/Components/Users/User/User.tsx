0cb7c9c542fc124a93ae7b098b7804d2ae032b6a
Artem Balanovskyi <mr.balanovsky@gmail.com>
2023-05-21 23:11:10 +0300

    feat: add users page markup

--- src/Components/Users/User/User.tsx
+++ src/Components/Users/User/User.tsx
@@ -1,12 +1,36 @@
 import s from "../Users.module.css";
 import React from "react";
-import {NavLink} from "react-router-dom";
 import {IUser} from "../../../redux/interfaces/usersPageState.interface";
 
 export const User = (user: IUser) => {
     return (
-        <div className={s.user + ' ' + s.active}>
-            <NavLink to={`/users/${user.id}`}>{user.fullName}</NavLink>
+        <div className={s.user_block}>
+            <div className={s.avatar_block}>
+                <div>
+                    <img className={s.avatar_block_img} src={user.avatar_url} alt="avatar_url"/>
+                </div>
+                <button className={s.avatar_block_button}>
+                    {user.followed ? 'Unfollow' : 'Follow'}
+                </button>
+            </div>
+            <div className={s.details_block}>
+                <div className={s.details_block_left}>
+                    <div className={s.details_block_fullName}>
+                        {user.fullName}
+                    </div>
+                    <div className={s.details_block_status}>
+                        {user.status}
+                    </div>
+                </div>
+                <div className={s.details_block_right}>
+                    <div className={s.details_block_country}>
+                        {user.location.country}
+                    </div>
+                    <div className={s.details_block_cityName}>
+                        {user.location.cityName}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
